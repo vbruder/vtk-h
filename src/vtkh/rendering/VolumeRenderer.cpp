de5e5d1515c2d7aba5315682a3d05dab623dc352
Valentin Bruder <vbruder@gmail.com>
2020-11-26 13:44:53 +0100

    variable supersampling

--- src/vtkh/rendering/VolumeRenderer.cpp
+++ src/vtkh/rendering/VolumeRenderer.cpp
@@ -540,7 +540,7 @@ VolumeRenderer::RenderOneDomainPerRank()
     const int size = width * height;
     const int color_size = size * color_stride;
 
-    const int supersampling = 2;    // This is per dimension. Set to 1 to disable supersampling.
+    const int supersampling = 3;    // This is per dimension. Set to 1 to disable supersampling.
     const int super_width = width*supersampling;
     const int super_height = height*supersampling;
     const int super_color_line = super_width * color_stride;
@@ -604,11 +604,19 @@ VolumeRenderer::RenderOneDomainPerRank()
             size_t super_id = j*super_color_line*supersampling + k*supersampling*color_stride;
             for (size_t c = 0; c < color_stride; ++c)  // RGBA
             {
-              float col = color_buffer[super_id + c];
-              col += color_buffer[super_id + c + color_stride];        // next element in line
-              col += color_buffer[super_id + c + super_color_line];    // next line
-              col += color_buffer[super_id + c + super_color_line + color_stride];
-              unsigned char final_color = static_cast<unsigned char>(int((col/color_stride) * 255.f));
+              size_t line_id = super_id;
+              float col = 0.f;
+              for (size_t kernel_j = 0; kernel_j < supersampling; ++kernel_j)
+              {
+                for (size_t kernel_i = 0; kernel_i < supersampling; ++kernel_i)
+                {
+                  col += color_buffer[line_id + kernel_i*color_stride + c];
+                }
+                line_id += super_color_line;
+              }
+
+              col /= (supersampling*supersampling);
+              unsigned char final_color = static_cast<unsigned char>(int(col * 255.f));
               m_color_buffers[i][j*width*color_stride + k*color_stride + c] = final_color;
             }
           }
